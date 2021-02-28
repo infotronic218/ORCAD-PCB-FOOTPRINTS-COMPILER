23e0ca5d744d332d69a2b7f8d24852da51434f50
infotronic218 <infotronic218@gmail.com>
2021-02-28 18:39:38 +0100

    Compiling to executable file for window OS

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import tkinter as tk
-from tkinter.ttk import Button, Label, Entry, Frame, Style
+from tkinter.ttk import Button, Label, Entry, Frame, Style, Progressbar
 from tkinter import filedialog
 from tkinter import messagebox
 
 from functools import partial
 from compile import Compiler
-from threading import Thread
+import threading
+
 
 
 class MainApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.destination_entry = None
+        self.source_entry = None
+        self.progressbar = None
         self.wm_title("Orcad PCB Library Compiler")
         self.geometry("500x250")
         self.configure(bg="#e9e4ed")
         self.resizable(False, False)
         self.center_the_screen()
-        self.iconbitmap(bitmap="./images/icon.ico")
+        try:
+            self.iconbitmap("D:\P2020\PYTHON\PCBLIB_COMPILER\images\icon.ico")
+        except:
+            pass
 
     def center_the_screen(self):
         window_width = self.winfo_reqwidth()
@@ -40,12 +47,16 @@ class MainApp(tk.Tk):
         if str(source_folder).__eq__("") or str(destination_folder).__eq__(""):
             messagebox.showerror(title="Compiling Error", message="Source and destination folders should not be empty!")
         else:
+            self.progressbar["mode"] = "indeterminate"
+            self.progressbar.start(20)
             def run():
                 compiler = Compiler(source_folder=source_folder, destination_folder=destination_folder)
                 compiler.compile()
+                self.progressbar.stop()
+                self.progressbar["mode"]="determinate"
+                self.progressbar["value"] =100
                 messagebox.showinfo(title="Completed", message="Libraries compiled successfully to one folder!")
-
-            Thread(target=run(), daemon=False).start()
+            threading.Thread(target=run, daemon=True).start()
 
     def open_folder_dialog(self, entry, text):
         directory = filedialog.askdirectory()
@@ -56,16 +67,19 @@ class MainApp(tk.Tk):
         main_frame = Frame(self, borderwidth=1)
         main_frame.pack(fill=tk.BOTH, expand=True)
 
-        source_label = Label(main_frame, text="Libraries parent folder :", anchor="e", justify=tk.LEFT)
+        source_label = Label(main_frame, text="Libraries parent folder ", anchor="e", justify=tk.LEFT)
         self.source_entry = Entry(main_frame, width=65)
         source_button = Button(main_frame, width=10, text="...")
         source_button.bind("<Button-1>", lambda event: self.open_folder_dialog(self.source_entry, "Destination"))
 
-        destination_label = Label(main_frame, text="Compiled destination folder :")
+        destination_label = Label(main_frame, text="Compiled destination folder ")
         self.destination_entry = Entry(main_frame, width=65)
         destination_button = Button(main_frame, width=10, text="...")
         destination_button.bind("<Button-1>", lambda event: self.open_folder_dialog(self.destination_entry,
                                                                                     "Select destination folder"))
+        progress_label = Label(main_frame, text="Compilation progress", anchor="e", justify=tk.LEFT)
+        self.progressbar = Progressbar(main_frame, orient=tk.HORIZONTAL,value="0", mode="indeterminate", length=400)
+        self.progressbar["mode"] = "determinate"
 
         source_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W, columnspan=2)
         self.source_entry.grid(row=1, column=1, padx=5, pady=5, columnspan=9)
@@ -74,6 +88,9 @@ class MainApp(tk.Tk):
         destination_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W, columnspan=2)
         self.destination_entry.grid(row=3, column=1, padx=5, pady=5, columnspan=9)
         destination_button.grid(row=3, column=10, padx=5, pady=5)
+
+        progress_label.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W, columnspan=2)
+        self.progressbar.grid(row=5, column=0, padx=5, pady=5,sticky=tk.W, columnspan =2)
 
     def add_bottom_frame(self):
         bottom_frame = Frame(self, borderwidth=1)
